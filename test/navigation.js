e45b0df648ba91260b69fc02ec96607d067109d1
Masayuki Nagamachi <masayuki.nagamachi@gmail.com>
2017-01-06 15:11:36 +0900

    Add copyright notice

--- test/navigation.js
+++ test/navigation.js
@@ -1,1 +1,7 @@
+//
+// Copyright (c) 2017 Masayuki Nagamachi <masayuki.nagamachi@gmail.com>
+//
+// This file is distributed under the MIT license.
+// See LICENSE file in the project root for details.
+
 module.exports.navigate = (driver) => driver;
